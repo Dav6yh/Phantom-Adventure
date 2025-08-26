8e318120e7e0297b16374ede871329a7aeb69b7c
Dav6yh <davyh6128@gmail.com>
2025-08-26 17:03:31 -0300

    Joistick

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -1,20 +1,26 @@
 using UnityEngine;
+using UnityEngine.InputSystem;
 using UnityEngine.U2D;
 
 public class Player : MonoBehaviour
 {
     [SerializeField] private int vida = 4; // Health of the player
     [SerializeField] private int dano;
-    [SerializeField] private float speedwalk;
-    [SerializeField] private float speedrun;
+    //[SerializeField] private float speedwalk;
+    //[SerializeField] private float speedrun;
     [SerializeField] private float forcejump;
     [SerializeField] private float moveH;
     [SerializeField] private bool noChao = true;
     [SerializeField] private int stelf = 3;
+
+    private VirtualJoystick2D joystick;
+
+    private Animator animator;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        animator = GetComponent<Animator>();
+        joystick = GameObject.Find("Background").GetComponent<VirtualJoystick2D>();
     }
 
     // Update is called once per frame
@@ -22,14 +28,38 @@ public class Player : MonoBehaviour
     {
         Mover();
         Jump();
+        if (noChao == true)
+        {
+            animator.SetBool("NoChao", true);
+            
+        }
+
     }
 
     private void Mover()
     {
 
-        moveH = Input.GetAxis("Horizontal");
-        transform.position += new Vector3(moveH * Time.deltaTime * speedwalk, 0, 0);
+        if (joystick.RetornaX() > 0)
+        {
+            animator.SetBool("Andando", false);
+            animator.SetBool("NoChao", true);
+            animator.SetTrigger("Andar");
+            GetComponent<SpriteRenderer>().flipX = false;
+        }
+        else if (joystick.RetornaX() < 0)
+        {
+            animator.SetBool("Andando", false);
+            animator.SetBool("NoChao", true);
+            animator.SetTrigger("Parar");
+            GetComponent<SpriteRenderer>().flipX = true;
 
+        }
+        else
+        {
+            animator.SetBool("Andando", true);
+            animator.SetBool("NoChao", true);
+            animator.SetTrigger("Parar");
+        }
     }
 
     private void Jump()
