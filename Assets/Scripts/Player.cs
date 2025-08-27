a64645ffb54f78decbf7d460e06e4fb741f996d2
Dav6yh <davyh6128@gmail.com>
2025-08-27 17:30:08 -0300

    Atualizacoes

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -8,9 +8,9 @@ public class Player : MonoBehaviour
     [SerializeField] private int dano;
     //[SerializeField] private float speedwalk;
     //[SerializeField] private float speedrun;
-    [SerializeField] private float forcejump;
+    //[SerializeField] private float forcejump;
     [SerializeField] private float moveH;
-    [SerializeField] private bool noChao = true;
+    //[SerializeField] private bool noChao = true;
     [SerializeField] private int stelf = 3;
 
     private VirtualJoystick2D joystick;
@@ -27,13 +27,6 @@ public class Player : MonoBehaviour
     void Update()
     {
         Mover();
-        Jump();
-        if (noChao == true)
-        {
-            animator.SetBool("NoChao", true);
-            
-        }
-
     }
 
     private void Mover()
@@ -41,33 +34,22 @@ public class Player : MonoBehaviour
 
         if (joystick.RetornaX() > 0)
         {
-            animator.SetBool("Andando", false);
-            animator.SetBool("NoChao", true);
+            animator.SetBool("Andando", true);
             animator.SetTrigger("Andar");
             GetComponent<SpriteRenderer>().flipX = false;
         }
         else if (joystick.RetornaX() < 0)
         {
-            animator.SetBool("Andando", false);
-            animator.SetBool("NoChao", true);
+            animator.SetBool("Andando", true);
             animator.SetTrigger("Parar");
             GetComponent<SpriteRenderer>().flipX = true;
 
         }
         else
         {
-            animator.SetBool("Andando", true);
-            animator.SetBool("NoChao", true);
+            animator.SetBool("Andando", false);
             animator.SetTrigger("Parar");
         }
-    }
-
-    private void Jump()
-    {
-      
-        GetComponent<Rigidbody2D>().AddForce(new Vector2(0, forcejump), ForceMode2D.Impulse);
-        noChao = false;
-        
     }
 
 }
---
+++ Assets/Scripts/PlayerController2D.cs
@@ -0,0 +1,77 @@
+using System.Security.Cryptography;
+using Unity.Mathematics;
+using UnityEngine;
+using UnityEngine.UI;
+
+[RequireComponent(typeof(Rigidbody2D))]
+public class PlayerController2D : MonoBehaviour
+{
+    [Header("Movement Settings")]
+    public VirtualJoystick2D joystick;
+    public float moveSpeed = 5f;
+
+    [Header("Jump Settings")]
+    [SerializeField] private float forcaPulo;
+    [SerializeField] private bool noPiso = true;
+    [Header("Components")]
+    private Rigidbody2D rb;
+    private Animator animator;
+
+    [Header("Buttons")]
+    public Button jumpButton;
+   
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        animator = GetComponent<Animator>();   
+    }
+
+    void FixedUpdate()
+    {
+        //Vector2 input = joystick != null ? joystick.InputDirection : new Vector2(Input.GetAxis("Horizontal"), 0);
+
+        //rb.linearVelocity = input * moveSpeed;
+    }
+
+    public void Pular()
+    {
+        if(noPiso == true)
+        {
+            rb.AddForce(Vector2.up * forcaPulo, ForceMode2D.Impulse);
+            //rb.linearVelocity *= forcaPulo;
+            animator.SetBool("NoChao", false);
+            animator.SetTrigger("Pular");
+            noPiso = false;
+        }
+            
+    }
+
+    //public void OnJumpButtonPressed()
+    //{
+    //    if (!apertado)
+    //    {
+    //        Pular();
+    //        apertado = true;
+    //    }
+    //}
+
+    void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.collider.CompareTag("Ground"))
+        {
+            noPiso = true;
+            animator.SetBool("NoChao", true);
+        }
+        
+    }
+
+    private void OnCollisionStay2D(Collision2D collision)
+    {
+        if (collision.collider.CompareTag("Ground"))
+        {
+            noPiso = true;
+            animator.SetBool("NoChao", true);
+        }
+    }
+}
